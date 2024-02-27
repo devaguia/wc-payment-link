1f430e466377601a3f2e5f5692d4008c59d4a733
Matheus Aguiar <aguiartgv@gmail.com>
2024-02-27 09:59:12 +0000

    build: Building a new version

--- .github/tag.sh
+++
@@ -1,8 +0,0 @@
-cat ./readme.txt | while read line
-do
-    if [[ "$line" == *"Stable tag:"* ]]; then
-        tag=$( echo "$line" | cut -d ':' -f 2 )
-        echo $tag
-        exit;
-    fi
-done
