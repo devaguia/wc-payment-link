2118c6b8b3096fc4c0efbe189b2cbdaf82f55dbd
Matheus Aguiar <matheus@aguia.dev>
2024-05-02 12:46:42 +0000

    build: building a new version

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'devaguia/wc-payment-link',
+        'pretty_version' => '1.0.3',
+        'version' => '1.0.3.0',
+        'reference' => null,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'devaguia/wc-payment-link' => array(
+            'pretty_version' => '1.0.3',
+            'version' => '1.0.3.0',
+            'reference' => null,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
